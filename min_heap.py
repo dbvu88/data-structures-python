9dad93f9d1eabb0be16aba161be482ca8d85c7fc
Duc Vu <de.yd315@gmail.com>
2019-07-20 21:29:39 +0000

    completed heap data structure implementation

--- min_heap.py
+++ min_heap.py
@@ -14,7 +14,29 @@ class MinHeap:
   def right_child_idx(self, idx):
     return idx * 2 + 1
 
+  # NEW HELPER!
+  def child_present(self, idx):
+    return self.left_child_idx(idx) <= self.count
+
   # END OF HEAP HELPER METHODS
+
+  def retrieve_min(self):
+    if self.count == 0:
+      print("No items in heap")
+      return None
+
+    min = self.heap_list[1]
+    print("Removing: {0} from {1}".format(min, self.heap_list))
+    self.heap_list[1] = self.heap_list[self.count]
+    # self.heap_list[self.count] = min
+    # print(self.heap_list)
+    self.heap_list.pop()
+    self.count -= 1
+    print("Last element moved to first: {0}".format(self.heap_list))
+    self.heapify_down()
+    print(self.heap_list)
+
+    return min
 
   def add(self, element):
     self.count += 1
@@ -22,17 +44,44 @@ class MinHeap:
     self.heap_list.append(element)
     self.heapify_up()
 
-  def heapify_up(self):
-    print("Heapifying up")
-    # start at the last element of the list
-    idx = len(self.heap_list) - 1
-    # while there's a parent element available:
-    while self.parent_idx(idx) > 0:
-      parent_idx = self.parent_idx(idx)
-      child = self.heap_list[idx]
-      parent = self.heap_list[parent_idx]
+
+  def heapify_down(self):
+    idx = 1
+    while self.child_present(idx):
+      smaller_child_idx = self.get_smaller_child_idx(idx)
+      child = self.heap_list[smaller_child_idx]
+      parent = self.heap_list[idx]
       if parent > child:
-        print("swapping parent_element with element_at_index")
-        self.heap_list[idx] = parent
-        self.heap_list[parent_idx] = child
-      idx = parent_idx
+        self.heap_list[idx] = child
+        self.heap_list[smaller_child_idx] = parent
+
+      idx = smaller_child_idx
+    return
+
+
+  def get_smaller_child_idx(self, idx):
+    if self.right_child_idx(idx) > self.count:
+      print("There is only a left child")
+      return self.left_child_idx(idx)
+    else:
+      left_child = self.heap_list[self.left_child_idx(idx)]
+      right_child = self.heap_list[self.right_child_idx(idx)]
+      if left_child < right_child:
+        print("Left child is smaller")
+        return self.left_child_idx(idx)
+      else:
+        print("Right child is smaller")
+        return self.right_child_idx(idx)
+
+  def heapify_up(self):
+    idx = self.count
+    while self.parent_idx(idx) > 0:
+      if self.heap_list[self.parent_idx(idx)] > self.heap_list[idx]:
+        tmp = self.heap_list[self.parent_idx(idx)]
+        print("swapping {0} with {1}".format(tmp, self.heap_list[idx]))
+        self.heap_list[self.parent_idx(idx)] = self.heap_list[idx]
+        self.heap_list[idx] = tmp
+      idx = self.parent_idx(idx)
+    print("HEAP RESTORED! {0}".format(self.heap_list))
+    print("")
+
