7badb4ad6084ed70c9f28a2f4b0b61ef69e3554a
Duc Vu <de.yd315@gmail.com>
2019-07-22 05:00:16 +0000

    updated

--- graph/graph.py
+++ graph/graph.py
@@ -1,7 +1,12 @@
 class Graph:
-  def __init__(self, directed = False):
-    self.graph_dict = {}
-    self.directed = directed
+    def __init__(self, directed=False):
+        self.graph_dict = {}
+        self.directed = directed
 
-  def add_vertex(self, vertex):
-    self.graph_dict[vertex.value] = vertex
+    def add_vertex(self, vertex):
+        self.graph_dict[vertex.value] = vertex
+
+    def add_edge(self, from_vertex, to_vertex):
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
+        if not self.directed:
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
